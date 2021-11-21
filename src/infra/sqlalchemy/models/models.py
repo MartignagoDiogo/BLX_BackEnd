c48b27b1a7b0470233a3c76944dda030c2b8091d
MartignagoDiogo <martignago.diogo@gmail.com>
2021-11-21 15:52:35 -0300

    Tabela Pedido

--- src/infra/sqlalchemy/models/models.py
+++ src/infra/sqlalchemy/models/models.py
@@ -2,6 +2,7 @@ from typing import Optional
 from pydantic.main import BaseModel
 from sqlalchemy import Column, String, Float, Integer
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import column
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import Boolean
 from src.infra.sqlalchemy.config.database import Base
@@ -17,6 +18,7 @@ class Usuario(Base):
     senha = Column(String)
     
     produtos = relationship('Produto', back_populates='usuario')
+    pedidos = relationship('Pedido', back_populates='usuario')
 
 
 class Produto(Base):
@@ -32,9 +34,19 @@ class Produto(Base):
     usuario_id = Column(Integer, ForeignKey('usuario.id', name='fk_usuario'))
     
     usuario = relationship('Usuario', back_populates='produtos')
-
-     
     
-
+class Pedido(Base):
+    __tablename__ = 'pedido'
     
+    id = Column(Integer, primary_key=True, index=True)
+    quantidade = Column(Integer)
+    local_entrega = Column(String)
+    tipo_entrega = Column(String)
+    observacao = Column(String)
+    
+    usuario_id = Column(Integer, ForeignKey('usuario.id', name='fk_usuario'))
+    produto_id = Column(Integer, ForeignKey('produto.id', name='fk_pedido_produto'))
+    
+    usuario = relationship('Usuario', back_populates='pedidos')
+    usuario = relationship('Usuario')
     
